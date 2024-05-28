1fa9685849d35260909b858636097dce4a8f41b7
jonelXpay <jonel@xpay.world>
2024-05-28 12:35:23 +0800

    #integration test

--- app/src/androidTest/java/com/jantiojo/usbconnectivity/usbconnection/UsbConnectionManagerTest.kt
+++ app/src/androidTest/java/com/jantiojo/usbconnectivity/usbconnection/UsbConnectionManagerTest.kt
@@ -7,9 +7,11 @@ import android.hardware.usb.UsbManager
 import androidx.test.platform.app.InstrumentationRegistry
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import junit.framework.TestCase.assertTrue
 import org.junit.Before
 import org.junit.Test
+import java.util.HashMap
 
 
 class UsbConnectionManagerTest {
@@ -50,5 +52,24 @@ class UsbConnectionManagerTest {
 
         // Verify that the device communication is set up
         assertTrue(::usbConnectionManager.isInitialized)
+    }
+
+    @Test
+    fun testOpenUsbConnection_failure() {
+        // Mock the USB manager to return an empty device list
+        val usbManager = mockk<UsbManager>()
+        every { usbManager.deviceList } returns hashMapOf()
+
+        val context = mockk<Context>()
+        every { context.getSystemService(Context.USB_SERVICE) } returns usbManager
+        every { usbConnectionManager.isUsbHostSupported(context) } returns true
+        usbConnectionManager.openUsbConnection()
+
+        // Verify that the listener is notified of the error
+        val listener = mockk<UsbCommunicationListener>()
+        usbConnectionManager.setUsbCommunicationListener(listener)
+
+
+        verify { listener.onError("No USB devices found.") }
     }
 }
